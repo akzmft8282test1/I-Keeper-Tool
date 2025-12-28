f26726621cdbe1835f0167bf218d6292cd95a9d0
akzmft <83045378+akzmft@users.noreply.github.com>
2025-12-28 19:01:56 +0900

    commit 9

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,11 @@
 import { Toaster } from "react-hot-toast";
-import Router from "./Router"; // 기존 라우터
+import Router from "./Router";
 
 function App() {
   return (
     <>
+      <Toaster position="top-right" />
       <Router />
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          duration: 3000,
-          style: {
-            fontSize: "14px"
-          }
-        }}
-      />
     </>
   );
 }
